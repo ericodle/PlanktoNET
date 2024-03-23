e29a88029c18b1f6abb568d60964b013f8088204
EricOdle <72859331+ericodle@users.noreply.github.com>
2024-03-23 14:49:42 +0900

    Update run_planktonet.py

--- run_planktonet.py
+++ run_planktonet.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append('./')
+sys.path.append('./src/')
 from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow, QMainWindow, QVBoxLayout, QLabel, QWidget, QFileDialog, QMessageBox, QScrollArea, QDialog, QVBoxLayout, QInputDialog
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
